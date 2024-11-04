4286c1a0a4fa53735afd4b02e7d7371e39bce441
KhromenkoDaniel <daniilkhro.study@gmail.com>
2024-11-04 21:48:59 +0200

    feat: add dummy data

---
+++ task1_groupby_function/dummy_data.ts
@@ -0,0 +1,7 @@
+export const data = [
+    { name: "Alice", age: 25, city: "New York", country: "USA" },
+    { name: "Bob", age: 30, city: "Los Angeles", country: "USA" },
+    { name: "Charlie", age: 25, city: "London", country: "UK" },
+    { name: "David", age: 35, city: "New York", country: "USA" },
+    { name: "Eve", age: 35, city: "Paris", country: "France" }
+];
